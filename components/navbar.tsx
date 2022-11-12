b02e775b144138a7301b5d5b3d06d21afa0e9a54
Jananadi Wedagedara <jananadi92@gmail.com>
2022-11-12 16:23:08 +0900

    improvement: Change order of the navigation items

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -6,16 +6,16 @@ export default function NavBar() {
             <div className="hidden md:block text-base">
                 <ul className="items-center justify-center space-y-8 md:flex md:space-x-6 md:space-y-0">
                     <li><Link href="/"><p className="hover:text-sky-600">Home</p></Link></li>
-                    <li><Link href="/projects"><p className="hover:text-sky-600">Projects</p></Link></li>
                     <li><Link href="/work"><p className="hover:text-sky-600">Work</p></Link></li>
+                    <li><Link href="/projects"><p className="hover:text-sky-600">Projects</p></Link></li>
                     <li><Link href="/about"><p className="hover:text-sky-600">About</p></Link></li>
                 </ul>
             </div>
             <div className="block md:hidden justify-center px-18 text-xl">
                 <ul className="justify-between flex items-center">
                     <li><Link href="/"><p>🏠</p></Link></li>
-                    <li><Link href="/projects"><p>👩🏻‍💻</p></Link></li>
                     <li><Link href="/work"><p>💼</p></Link></li>
+                    <li><Link href="/projects"><p>👩🏻‍💻</p></Link></li>
                     <li><Link href="/about"><p>👩🏻</p></Link></li>
                 </ul>
             </div>
