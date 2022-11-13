a0ad230df808d824928f41f0504b0d1693ee2a16
Jananadi Wedagedara <jananadi92@gmail.com>
2022-11-13 12:34:14 +0900

    Revert "style: Fix min height"
    This reverts commit 9c22c1005dcde7153f85a6493537a1f15a6dba64.

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,7 +10,7 @@ export default function Home() {
         <link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>👩🏻</text></svg>"></link>
       </Head>
 
-      <main className="py-16 flex flex-1 flex-col items-center align-center">
+      <main className="min-h-screen py-16 flex flex-1 flex-col items-center align-center">
         <h3 className="m-0 leading-tight text-5xl text-center">
           Hello! 👋🏼 
         </h3>
