4d3b42d84ebb10469fb9a22db10ca17151d4953d
Jananadi Wedagedara <jananadi92@gmail.com>
2022-11-16 18:41:37 +0900

    improvement: update index page intro

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,36 +5,56 @@ export default function Home() {
   return (
     <div className="px-6 dark:text-slate-300">
       <main className="h-full pt-16 pb-20 align-center">
-        <h3 className="m-0 leading-tight text-5xl text-center">
-          Hello! 👋🏼 
-        </h3>
-        
+        <h3 className="m-0 leading-tight text-5xl text-center">Hello! 👋🏼</h3>
+
         <p className="m-16 leading-6 text-center text-xl">
-          I'm Jananadi, a Full-Stack Developer <br />
-          based in Seoul, South Korea. 
+          I'm Jananadi, an Expat Full-Stack Developer <br />
+          based in Seoul, South Korea.
         </p>
 
         <div className="max-w-5xl flex flex-wrap items-center justify-center">
-          <Link href="/work" className="max-w-md m-4 p-6 text-left inherit no-underline border border-gray-200 rounded-xl hover:text-blue-600 hover:border-blue-600">
+          <Link
+            href="/work"
+            className="max-w-md m-4 p-6 text-left inherit no-underline border border-gray-200 rounded-xl hover:text-blue-600 hover:border-blue-600"
+          >
             <h2 className="mb-4 text-2xl">Work 💼 </h2>
-            <p className="mb-5 text-lg">Check out some work projects that I was a part of over last few years. They taught me a lot. </p>
+            <p className="mb-5 text-lg">
+              Check out some work projects that I was a part of over last few
+              years. They taught me a lot.{' '}
+            </p>
           </Link>
 
-          <Link href="/projects" className="max-w-md m-4 p-6 text-left inherit no-underline border border-gray-200 rounded-xl hover:text-blue-600 hover:border-blue-600">
+          <Link
+            href="/projects"
+            className="max-w-md m-4 p-6 text-left inherit no-underline border border-gray-200 rounded-xl hover:text-blue-600 hover:border-blue-600"
+          >
             <h2 className="mb-4 text-2xl">Projects | Writing 👩🏻‍💻</h2>
-            <p className="mb-5 text-lg">Here are few projects I did and currently working on, and some articles.</p>
-          </Link>
-  
-          <Link href="https://www.canvasconfetti.com" className="max-w-md m-4 p-6 text-left inherit no-underline border border-gray-200 rounded-xl hover:text-blue-600 hover:border-blue-600">
-            <h2 className="mb-4 text-2xl">Paintings 🙈</h2>
-            <p className="mb-5 text-lg">Beside coding, I love to paint. Still an amateur, checkout some of my art.</p>
+            <p className="mb-5 text-lg">
+              Here are few projects I did and currently working on, and some
+              articles.
+            </p>
           </Link>
 
-          <Link href="/about" className="max-w-md m-4 p-6 text-left inherit no-underline border border-gray-200 rounded-xl hover:text-blue-600 hover:border-blue-600">
+          <Link
+            href="https://www.canvasconfetti.com"
+            className="max-w-md m-4 p-6 text-left inherit no-underline border border-gray-200 rounded-xl hover:text-blue-600 hover:border-blue-600"
+          >
+            <h2 className="mb-4 text-2xl">Paintings 🙈</h2>
+            <p className="mb-5 text-lg">
+              Beside coding, I love to paint. Still an amateur, checkout some of
+              my art.
+            </p>
+          </Link>
+
+          <Link
+            href="/about"
+            className="max-w-md m-4 p-6 text-left inherit no-underline border border-gray-200 rounded-xl hover:text-blue-600 hover:border-blue-600"
+          >
             <h2 className="mb-4 text-2xl">About 👩🏻‍💼</h2>
             {/* &rarr; -> */}
             <p className="mb-5 text-lg">
-              Let me introduce myself. Find my contact details here as well. Thanks for visiting!
+              Let me introduce myself. Find my contact details here as well.
+              Thanks for visiting!
             </p>
           </Link>
         </div>
