304217500dc5a3c57a702ff91260598269c80211
Jananadi Wedagedara <jananadi92@gmail.com>
2022-11-16 18:02:34 +0900

    improvement: Add image meta tag in the header

--- components/header.tsx
+++ components/header.tsx
@@ -6,11 +6,8 @@ export default function Header() {
   return (
     <Head>
       <title>Jananadi W</title>
-      <meta
-        name="description"
-        content="/public/preview.png"
-        property="og:image"
-      />
+      <meta name="description" content="Developer portfolio site" />
+      <meta name="image" content="/public/preview.png" property="og:image" />
       <link
         rel="icon"
         href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>👩🏻</text></svg>"
