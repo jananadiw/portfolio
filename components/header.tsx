43b7f674189071e401bfe948d1a238383c492b58
Jananadi Wedagedara <jananadi92@gmail.com>
2022-11-16 18:12:13 +0900

    fix: Update image tag

--- components/header.tsx
+++ components/header.tsx
@@ -7,7 +7,11 @@ export default function Header() {
     <Head>
       <title>Jananadi W</title>
       <meta name="description" content="Developer portfolio site" />
-      <meta name="image" content="/public/preview.png" property="og:image" />
+      <meta
+        name="image"
+        content="https://jananadi.me/public/preview.png"
+        property="og:image"
+      />
       <link
         rel="icon"
         href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>👩🏻</text></svg>"
