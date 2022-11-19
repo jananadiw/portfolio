d3ec80ea0f06a1e6c9d38438a7d9464097cf1c3d
Jananadi Wedagedara <jananadi92@gmail.com>
2022-11-20 03:31:03 +0900

    style: Add style for active links

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,39 +1,38 @@
 import Link from 'next/link'
 import useTransition from 'next-translate/useTranslation'
+import { useRouter } from 'next/router'
 
-// const menuItems = [
-//   { id: 'home', name: 'nav_home' , path: '/'},
-//   { id: 'work', name: 'nav_work' , path: '/work'},
-//   { id: 'projects', name: 'nav_projects' , path: '/projects'},
-//   { id: 'about', name: 'nav_about' , path: '/about'},
-// ]
+const menuItems = [
+  { id: 'home', name: 'nav_home', path: '/' },
+  { id: 'work', name: 'nav_work', path: '/work' },
+  { id: 'projects', name: 'nav_projects', path: '/projects' },
+  { id: 'about', name: 'nav_about', path: '/about' },
+]
 
 export default function NavBar() {
   const { t } = useTransition('common')
+  const router = useRouter()
   return (
     <div className="m-8 p-8 dark:text-slate-300 h-16 items-center align-center ">
       <div className="hidden md:block text-base">
         <ul className="items-center justify-center space-y-8 md:flex md:space-x-6 md:space-y-0">
-          <li>
-            <Link href="/">
-              <p className="hover:text-indigo-400">{t('nav_home')}</p>
-            </Link>
-          </li>
-          <li>
-            <Link href="/work">
-              <p className="hover:text-indigo-400">{t('nav_work')}</p>
-            </Link>
-          </li>
-          <li>
-            <Link href="/projects">
-              <p className="hover:text-indigo-400">{t('nav_projects')}</p>
-            </Link>
-          </li>
-          <li>
-            <Link href="/about">
-              <p className="hover:text-indigo-400">{t('nav_about')}</p>
-            </Link>
-          </li>
+          {menuItems.map((menu) => {
+            return (
+              <li key={menu.id}>
+                <Link href={menu.path}>
+                  <p
+                    className={
+                      menu.path === router.pathname
+                        ? 'text-indigo-400 hover:text-indigo-300'
+                        : 'text-base hover:text-indigo-400'
+                    }
+                  >
+                    {t(`${menu.name}`)}
+                  </p>
+                </Link>
+              </li>
+            )
+          })}
         </ul>
       </div>
       <div className="block md:hidden justify-center px-18 text-xl">
