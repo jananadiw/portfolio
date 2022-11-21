5f749cb762724f283789cdaa736161bdbeaf5b7b
Jananadi Wedagedara <jananadi92@gmail.com>
2022-11-21 12:42:36 +0900

    improvement: Set i18N detect locale flag to true

--- i18n.js
+++ i18n.js
@@ -2,7 +2,7 @@
 module.exports = {
     locales: ['en-US', 'kr'], // Array with the languages that you want to use
     defaultLocale: 'en-US', // Default language of your website
-    // localeDetection: true,
+    localeDetection: true,
     pages: {
       '*': ['common'], // Namespaces that you want to import per page (we stick to one namespace for all the application in this tutorial)
       '/about': ['about'],
